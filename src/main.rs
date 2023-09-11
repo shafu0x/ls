7e959f109a2479d3a678e1912720a6c25e3cb958
shafu <selfouly@gmail.com>
2023-09-11 22:58:39 +0200

    colors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::fs;
-use std::io;
 use std::fs::DirEntry;
+use std::io;
 use std::os::unix::fs::PermissionsExt;
+
+extern crate colored;
+use colored::*;
 
 fn is_directory(entry: &DirEntry) -> bool {
     if let Ok(file_type) = entry.file_type() {
@@ -21,12 +24,9 @@ fn parse_permissions(entry: &DirEntry) -> io::Result<String> {
     let permissions = metadata.permissions();
     let mode = permissions.mode();
     let mut result = String::new();
-    result.push(if mode & 0o400 != 0 { 'r' } else { '-' });
-    result.push(if mode & 0o200 != 0 { 'w' } else { '-' });
     result.push(if mode & 0o100 != 0 { 'x' } else { '-' });
     Ok(result)
 }
-
 
 fn main() {
     let mut entries: Vec<DirEntry> = fs::read_dir(".")
@@ -38,12 +38,17 @@ fn main() {
     entries.sort_by(|a, b| a.file_name().cmp(&b.file_name()));
 
     for entry in entries.iter() {
+        let is_dir = is_directory(entry);
         println!(
-            "{:<1} {:<5} {:<8} {}",
-            if is_directory(entry) { "d" } else { "" },
+            "{}{:<2} {:<6} {}",
+            if is_dir { "d" } else { "-" },
             parse_permissions(entry).unwrap_or(String::from("---------")),
-            get_file_size(entry).unwrap_or(0), 
-            entry.file_name().to_string_lossy()
+            get_file_size(entry).unwrap_or(0),
+            entry.file_name().to_string_lossy().color(if is_dir {
+                Color::Blue
+            } else {
+                Color::White
+            })
         );
     }
 }
